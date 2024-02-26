30b048c7cf0ab8e1fcca7fcc84ae107b99c66383
Lucas Ale <73314594+lucasmartinale@users.noreply.github.com>
2024-02-25 23:51:33 -0300

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BasicApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicApi.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
+
+        public DbSet<Photos> Photos { get; set; }
+    }
+}
---
+++ Models/Photos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicApi.Models
+{
+    public class Photos
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Photographer { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+    }
+}
